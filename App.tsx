f3d3d56526a0d9eaec958899a4cd4718293a6f63
inust33 <inust33@gmail.com>
2021-10-19 03:48:12 +0900

    challenge #11

--- App.tsx
+++ App.tsx
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from "expo-app-loading";
+import { Ionicons } from '@expo/vector-icons';
+import * as Font from 'expo-font';
+import {Asset} from "expo-asset";
+
+
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish= ()=>setLoading(false);
+  const preload = async ()=>{
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font)=>Font.loadAsync(font));
+    const imagesToLoad = [require("./assets/splash.png"), "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/840px-Instagram_logo.svg.png",]
+    const imagePromises = imagesToLoad.map(image=>Asset.loadAsync(image));
+    const newPromises = [...imagePromises,...fontPromises];
+
+    await Promise.all<Promise<void>|Promise<Asset[]>>([...fontPromises,...imagePromises]);
+
+  }
+  if(loading){
+    return <AppLoading onError={console.warn}  onFinish={onFinish} startAsync={preload} />
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
